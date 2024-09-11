11d2fdf4c15e04143127bb7d217d09f5df60dbeb
David Jachym <jachym19@gmail.com>
2024-09-10 23:16:35 -0500

    added password length limit; formatted output

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,13 @@
 import random
+from colorama import Fore, Style
+from tabulate import tabulate
 
 
 def main():
+    print(f"{Fore.YELLOW}{'PASSWORD GENERATOR'.center(36, '=')}{Style.RESET_ALL}")
     password_length = get_password_length()
     password = generate_password(password_length)
-    print("".join(password))
+    display_password(password)
 
 
 def get_password_length():
@@ -13,6 +16,8 @@ def get_password_length():
             password_length = int(input("Enter desired password length: "))
             if password_length < 14:
                 print("ERROR: Strong password should be 14 characters or more")
+            elif password_length > 50:
+                print("ERROR: Keep password length <= 50 characters")
             else:
                 return password_length
 
@@ -34,11 +39,9 @@ def generate_password(password_length):
     counter = 0
 
     while counter < password_length:
-        # chooses which category to grab from
         random_number = generate_random_number(4)
 
         if random_number == 1:
-            # get a letter from letters list and append to password
             index = generate_random_number(len(lowercase_letters) - 1)
             password.append(lowercase_letters[index])
 
@@ -57,6 +60,11 @@ def generate_password(password_length):
         counter += 1
 
     return password
+
+
+def display_password(password):
+    table = [[f"{Fore.GREEN}{"".join(password)}{Style.RESET_ALL}"]]
+    print("\n" + tabulate(table, headers=[f"{Fore.RED}GENERATED PASSWORD{Style.RESET_ALL}"], tablefmt="github"))
 
 
 if __name__ == "__main__":
